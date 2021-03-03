dbaf769295aefa5ab0b2f6a0e244b3284187371f
Anton Litus <littox90@gmail.com>
2021-03-03 03:13:25 +0300

    dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
+use App\Models\Person;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Group::factory()
+            ->count(1)
+            ->has(Person::factory()->count(5), 'persons')
+            ->has(Person::factory()->count(3)->inactive(), 'persons')
+            ->create();
+        Group::factory(1)
+            ->archived()
+            ->has(Person::factory()->count(5), 'persons')
+            ->create();
         // \App\Models\User::factory(10)->create();
     }
 }
